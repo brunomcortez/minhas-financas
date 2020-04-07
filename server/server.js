55bd9ddc5fad8132e9a737f737b04de25a3ac299
Bruno Cortez <bruno.mcortez@gmail.com>
2020-04-06 23:56:55 -0300

    Revert "Server"
    This reverts commit d74a8ec4446f9d77125d029531120a5278d86361.

--- server/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http'),
-    app = require('./config/express');
-
-http.createServer(app).listen(3920, function() {
-    console.log('Server running on port: ' + this.address().port);
-});
-
